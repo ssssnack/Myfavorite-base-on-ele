8e3dfb23e3ec1988a62135eb00efaeb90be7d343
ssssnack <247770638@qq.com>
2018-03-13 15:57:39 +0800

    列表组件化

--- src/components/mList/mList.vue
+++ src/components/mList/mList.vue
@@ -1,18 +1,83 @@
 <template>
- <div class="list-view">
-
- </div>
+<el-row :gutter="30" class="list" :mType="mType">
+        <el-col :span="6" v-for="item in data" 
+        class="list-item">
+          <el-row class="grid-content" :class="[mType]" :gutter="10">
+              <el-col :span="12">
+                <img :src="item.cover" width="115" height="173">
+              </el-col>
+              <el-col :span="12" class="text">
+                <div class="title">{{item.title}}</div>
+                <div class="rate"> {{item.rate}}
+                  <span class="small">分</span>
+                  </div>
+              </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
 </template>
 
 <script>
 export default {
-  data() {
-    return {};
-  },
-  components: {}
+  //接受数据
+  props: {
+    data: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    },
+    mType: {
+      type: String,
+      default: 'bg-movie'
+    }
+  }
 };
 </script>
 
-<style>
+<style lang="scss" scoped >
+.list {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+  .list-item {
+    border-radius: 4px;
+    margin-bottom: 20px;
+    .text {
+      padding-top: 50px;
+      margin-left: -15px;
+      .title {
+        font-size: 20px;
+        color: #a2a7ad;
+        text-align: center;
+        font-weight: lighter;
+      }
+      .rate {
+        color: #a6924b;
+        text-align: center;
+        font-size: 20px;
+        margin-top: 15px;
+        font-weight: lighter;
+        .small {
+          font-size: 14px;
+        }
+      }
+    }
+  }
+}
 
+
+.bg-movie {
+  background: #f2f6fc;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 184px;
+  padding-top: 5px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
 </style>
--- src/view/books/books.vue
+++ src/view/books/books.vue
@@ -1,23 +1,29 @@
 <template>
- <div>
-推荐图书
+ <div class="book-panel">
+  <h2 class="title">最受关注图书推荐 / 虚构类</h2>
  </div>
 </template>
 
 <script>
- export default {
-   data () {
-     return {
-
-     }
-   },
-   components: {
-
-   }
- }
+export default {
+  data() {
+    return {};
+  },
+  components: {}
+};
 </script>
 
-<style>
+<style lang="scss" scoped >
+.book-panel {
+  margin-top: 10px;
 
- 
+  .title {
+    font-family: "PingFang SC";
+    font-size: 15px;
+    color: #333;
+    font-weight: lighter;
+    padding: 10px 0px;
+  }
+}
+
 </style>
